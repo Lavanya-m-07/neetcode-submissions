68a87f8da94474f148d25110594cff5c638d5ba8
Lavanya-m-07 <mail2lavanya07@gmail.com>
2026-05-23 14:35:50 +0530

    Add: unique-email-addresses - submission-0

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+
+         unordered_set<string> st;
+
+        for(string email : emails)
+        {
+            int atPos = email.find('@');
+
+            string local = email.substr(0, atPos);
+            string domain = email.substr(atPos);
+
+            string cleaned = "";
+
+            for(char c : local)
+            {
+                if(c == '+')
+                    break;
+
+                if(c != '.')
+                    cleaned += c;
+            }
+
+            st.insert(cleaned + domain);
+        }
+
+        return st.size();
+        
+    }
+};
