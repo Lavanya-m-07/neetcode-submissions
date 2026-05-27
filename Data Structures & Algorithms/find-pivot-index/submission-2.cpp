9fdc3fd226e3ddb3719fbe4cd25207bcdaacad76
Lavanya-m-07 <mail2lavanya07@gmail.com>
2026-05-27 11:12:30 +0530

    Add: find-pivot-index - submission-2

---
+++ Data Structures & Algorithms/find-pivot-index/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        //calculating the total sum ;
+        int totalSum = 0;
+        for(int num:nums)
+        {
+            totalSum+=num;
+        }
+        int leftSum = 0;
+        int rightSum = 0;
+        for(int i = 0; i <nums.size();i++)
+        {
+          rightSum = totalSum-leftSum-nums[i];
+          if(leftSum==rightSum)
+          {
+            return i;
+          }
+          leftSum+=nums[i];
+        }
+
+        return -1;
+    
+    }
+};
