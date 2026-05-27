afef8a85220b1b266a69a024e1a4966a2d7e1282
Lavanya-m-07 <mail2lavanya07@gmail.com>
2026-05-27 11:45:25 +0530

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+          if(strs.empty())
+          {
+            return "";
+          }
+          sort(strs.begin(),strs.end());
+          string first = strs.front();
+          string second = strs.back();
+          int i = 0;
+          while(i < first.size() && i < second.size() && first[i]==second[i])
+          {
+            i++;
+          }
+
+          return first.substr(0,i);
+    }
+};
