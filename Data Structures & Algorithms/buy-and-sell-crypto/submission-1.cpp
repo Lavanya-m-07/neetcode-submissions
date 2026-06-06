86235e45998da6f0a1191165aa8cafa372463ff3
Lavanya-m-07 <mail2lavanya07@gmail.com>
2026-06-06 22:12:36 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buyPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 0; i < prices.size(); i++)
+        {
+            buyPrice = min(buyPrice,prices[i]);
+            int profit = prices[i]-buyPrice;
+            maxProfit = max(maxProfit,profit);
+        }
+        return maxProfit;
+    }
+};
