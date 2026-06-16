0bc57c329c80299d970832022f72f3d26c6c8dca
Lavanya-m-07 <mail2lavanya07@gmail.com>
2026-06-16 23:56:20 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> mp;
+
+        // Step 1: Count frequency
+        for(int n : nums)
+        {
+            mp[n]++;
+        }
+
+        // Step 2: Create buckets
+        vector<vector<int>> bucket(nums.size() + 1);
+
+        // Put numbers into their frequency bucket
+        for(auto it : mp)
+        {
+            int num = it.first;
+            int freq = it.second;
+
+            bucket[freq].push_back(num);
+        }
+
+        vector<int> result;
+
+        // Step 3: Traverse from highest frequency
+        for(int i = bucket.size() - 1; i >= 0; i--)
+        {
+            for(int n : bucket[i])
+            {
+                result.push_back(n);
+
+                if(result.size() == k)
+                {
+                    return result;
+                }
+            }
+        }
+
+        return result;    }
+};
