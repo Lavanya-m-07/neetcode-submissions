ebd73a02eaee92852bcb2ef302cefd5ae1fac433
Lavanya-m-07 <mail2lavanya07@gmail.com>
2026-09-05 14:11:38 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.4
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "sonygolu"
+print(type(variable))
+variable = [12,13]
+print(type(variable))
