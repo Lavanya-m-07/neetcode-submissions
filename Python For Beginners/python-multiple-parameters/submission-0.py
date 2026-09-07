1e6f4e871c98b3c2bc3e4ee0c2fdb20f03550bd7
Lavanya-m-07 <mail2lavanya07@gmail.com>
2026-09-07 21:36:09 +0530

    Add: python-multiple-parameters - submission-0

---
+++ Python For Beginners/python-multiple-parameters/submission-0.py
@@ -0,0 +1,12 @@
+def two_sum(a,b):
+    print(a+b)
+
+def three_sum(a,b,c):
+    print(a+b+c)
+
+two_sum(10,7)
+three_sum(9,1,6)
+
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)
